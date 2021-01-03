0b288a572062c043809f1b90feffc38e572bb6e8
Justin Hubbell <justin@jwhubbell.com>
2021-01-02 18:43:26 -0600

    added basic layout for html elements

--- silvasite/src/App.js
+++ silvasite/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <Container>
+        <Container>
+          <Row>
+            <Col></Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <Container></Container>
+          </Row>
+        </Container>
+      </Container>
     );
   }
 }
